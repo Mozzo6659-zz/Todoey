1407aa65db77a2b6e73d1e7f118de7f0bca88968
Mick Mossman <mossmanm@macbooks-macbook-pro.local>
2018-08-24 15:32:14 +1000

    Add tableview

---
+++ Todoey/TodolistViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Mick Mossman on 24/8/18.
+//  Copyright © 2018 Mick Mossman. All rights reserved.
+//
+
+import UIKit
+
+class TodolistViewController: UITableViewController {
+
+    let itemArray = ["Get bog roll","Get Apples","Fricking Avacodo"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview shite
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    //MARK - Tableview delegate methinds
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
+
